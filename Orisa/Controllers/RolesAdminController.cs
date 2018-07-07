0eff0746232a5c112fe74fdd8cb5c28ba5990b8c
James Kamana <jaykamana@gmail.com>
2018-07-07 16:03:22 +0300

    Update details and create controllers.

--- Orisa/Controllers/RolesAdminController.cs
+++ Orisa/Controllers/RolesAdminController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using Orisa.Models;
+using Orisa.ViewModel;
 
 namespace Orisa.Controllers
 {
@@ -52,9 +57,26 @@ namespace Orisa.Controllers
         }
 
         // GET: RolesAdmin/Details/5
-        public ActionResult Details(int id)
+        public async Task<ActionResult> Details(string id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var role = await RoleManager.FindByIdAsync(id);
+            // Get the list of Users in this Role
+            var users = new List<ApplicationUser>();
+            // Get the list of Users in this Role
+            foreach (var user in UserManager.Users.ToList())
+            {
+                if (await UserManager.IsInRoleAsync(user.Id, role.Name))
+                {
+                    users.Add(user);
+                }
+            }
+            ViewBag.Users = users;
+            ViewBag.UserCount = users.Count();
+            return View(role);
         }
 
         // GET: RolesAdmin/Create
@@ -65,18 +87,20 @@ namespace Orisa.Controllers
 
         // POST: RolesAdmin/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public async Task<ActionResult> Create(RoleViewModel roleViewModel)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add insert logic here
-
+                var role = new IdentityRole(roleViewModel.Name);
+                var roleresult = await RoleManager.CreateAsync(role);
+                if (!roleresult.Succeeded)
+                {
+                    ModelState.AddModelError("", roleresult.Errors.First());
+                    return View();
+                }
                 return RedirectToAction("Index");
             }
-            catch
-            {
-                return View();
-            }
+            return View();
         }
 
         // GET: RolesAdmin/Edit/5
